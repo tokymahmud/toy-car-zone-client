2e3a231429a2721964c5cf2ac64ba095d99556de
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-18 18:26:14 +0600

    login logout based on user

--- src/Pages/Shared/NavBar/Navbar.jsx
+++ src/Pages/Shared/NavBar/Navbar.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Providers/authProviders';
 
 const Navbar = () => {
+    const {user,logOut} =useContext(AuthContext);
+    const handleLogOut=() =>{
+        logOut()
+        .then(()=>{})
+        .catch(error=>console.log(error))
+
+
+    }
+
     const NavItems = <>
     <li><Link to='/'>Home</Link></li>
     <li><Link to='/alltoys'>All Toys</Link></li>
-    {/* <li>My Toys</li>
-    <li>Add A Toy</li> */}
+    
     <li><Link to='/blogs'>Blogs</Link></li>
+    {
+    user? <>
+    <span>{user.img}</span> 
+    <button onClick={handleLogOut} className="btn btn-outline">Log Out</button>
+    <li><Link to='/mytoys'>My Toys</Link></li>
+    <li><Link to='/addatoy'>Add A Toy</Link></li>
+
+    </>
+    :<>
     <li><Link to='/login'>Login</Link></li>
+    
+   
+    </>
+ }
     
     </>
     return (
