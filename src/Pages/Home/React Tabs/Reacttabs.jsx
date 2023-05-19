639fa2d74719eadf391610bdeabdc2899eee0a57
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-20 00:02:43 +0600

    toycode route for view details

--- src/Pages/Home/React Tabs/Reacttabs.jsx
+++ src/Pages/Home/React Tabs/Reacttabs.jsx
@@ -6,7 +6,7 @@ import 'react-tabs/style/react-tabs.css';
 const Reacttabs = () => {
   const [toyInfo, settoyInfo]=useState([]);
   useEffect(()=>{
-    fetch("../../../../public/toy.json")
+    fetch("http://localhost:5000/services")
     .then((response)=>response.json())
     .then((data)=>settoyInfo(data));
     
@@ -15,15 +15,15 @@ const Reacttabs = () => {
 
   },[]);
 
-
+console.log(toyInfo)
 
 
   return (
     <div>
         <h1 className='text-2xl text-center	decoration-emerald-500	font-bold'>Shop By <span className='underline '>Catagory</span></h1>
         <div className='flex items-center	'>
-        {toyInfo?.categories?.map(category => (
-        <Tabs key={category._id}>
+        {toyInfo.categories?.map(category => (
+        <Tabs key={category.id}>
           <TabList>
             <Tab>{category.name}</Tab>
           </TabList>
@@ -37,7 +37,10 @@ const Reacttabs = () => {
                 <h2 className="card-title">{item.toyName}</h2>
                 <p>Price: ${item.price}</p>
                 <p>Rating: {item.rating}</p>
-                <button  type="button" className="btn btn-primary" ><Link >View Details</Link> </button>
+                <Link to={`/services/${item.toycode}`}>
+                <button  type="button" className="btn btn-primary" >View Details </button>
+
+                </Link>
                 </div>
 
               </div>
